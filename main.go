6209a61b4e60a41da302efa953d5f1ff396deed7
Gosuke Miyashita <gosukenator@gmail.com>
2022-03-12 19:44:53 +0900

    Temporary commit of go-git version

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-billy/v5/util"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -29,19 +36,13 @@ type Block struct {
 func main() {
 	rootCmd := &cobra.Command{
 		Run: func(c *cobra.Command, args []string) {
-			base, err := c.PersistentFlags().GetString("base")
+			baseBranch, err := c.PersistentFlags().GetString("base")
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			target, err := c.PersistentFlags().GetString("target")
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			err = diff(base, target)
+			err = diff(baseBranch)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
@@ -50,7 +51,6 @@ func main() {
 	}
 
 	rootCmd.PersistentFlags().StringP("base", "b", "", "base branch")
-	rootCmd.PersistentFlags().StringP("target", "t", "", "target branch")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -58,7 +58,7 @@ func main() {
 	}
 }
 
-func diff(baseBranch, targetBranch string) error {
+func diff(baseBranch string) error {
 	if baseBranch == "" {
 		_, err := exec.Command("sh", "-c", "git branch | grep -q main").Output()
 		if err == nil {
@@ -75,39 +75,25 @@ func diff(baseBranch, targetBranch string) error {
 		}
 	}
 
-	if targetBranch == "" {
-		t, err := exec.Command("sh", "-c", "git rev-parse --abbrev-ref HEAD").Output()
-		if err != nil {
-			return err
-		}
-		targetBranch = strings.TrimSpace(string(t))
-	}
-
-	stash, err := exec.Command("sh", "-c", "git stash").Output()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	stashed := true
-	if strings.Contains(string(stash), "No local changes to save") {
-		stashed = false
-	}
-
-	_, err = exec.Command("sh", "-c", fmt.Sprintf("git checkout %s", baseBranch)).Output()
+	p, err := exec.Command("sh", "-c", "git rev-parse --show-prefix").Output()
 	if err != nil {
 		return err
 	}
+	path := strings.TrimSpace(string(p))
 
-	baseResources := parse()
-
-	exec.Command("sh", "-c", fmt.Sprintf("git checkout %s", targetBranch)).Output()
-
-	if stashed {
-		exec.Command("sh", "-c", "git stash pop").Output()
+	// Get resources on the base branch
+	content, err := getContent(baseBranch, path)
+	if err != nil {
+		return err
 	}
+	baseResources := parse(content)
 
-	targetResources := parse()
+	// Get resources on the target branch
+	content, err = getContent("", path)
+	if err != nil {
+		return err
+	}
+	targetResources := parse(content)
 
 	var differentResources []string
 
@@ -139,29 +125,82 @@ func diff(baseBranch, targetBranch string) error {
 	return nil
 }
 
-func parse() map[string]*Resource {
-	parser := hclparse.NewParser()
+func getContent(baseBranch, path string) ([]byte, error) {
+	var content []byte
 
-	files, err := filepath.Glob("*.tf")
-	if err != nil {
-		fmt.Println(err)
+	if baseBranch == "" {
+		files, err := filepath.Glob("*.tf")
+		if err != nil {
+			return content, nil
+		}
+
+		var buf bytes.Buffer
+		for _, f := range files {
+			c, err := ioutil.ReadFile(f)
+			if err != nil {
+				return content, err
+			}
+			buf.Write(c)
+		}
+
+		return buf.Bytes(), nil
+	} else {
+		r, err := exec.Command("sh", "-c", "git rev-parse --show-toplevel").Output()
+		if err != nil {
+			return content, err
+		}
+		root := strings.TrimSpace(string(r))
+
+		storer := memory.NewStorage()
+		fs := memfs.New()
+
+		repo, err := git.Clone(storer, fs, &git.CloneOptions{
+			URL: root,
+		})
+		if err != nil {
+			return content, err
+		}
+
+		w, err := repo.Worktree()
+		if err != nil {
+			return content, err
+		}
+
+		err = w.Checkout(&git.CheckoutOptions{Branch: plumbing.NewBranchReferenceName(baseBranch)})
+
+		files, err := util.Glob(fs, fmt.Sprintf("%s*.tf", path))
+		if err != nil {
+			return content, err
+		}
+
+		var buf bytes.Buffer
+		for _, f := range files {
+			c, err := util.ReadFile(fs, f)
+			if err != nil {
+				return content, err
+			}
+			buf.Write(c)
+		}
+
+		return buf.Bytes(), nil
+	}
+
+	return content, nil
+}
+
+func parse(content []byte) map[string]*Resource {
+	resources := make(map[string]*Resource)
+	parser := hclparse.NewParser()
+	file, parseDiags := parser.ParseHCL(content, "")
+	if parseDiags.HasErrors() {
+		fmt.Println(parseDiags.Error())
 		os.Exit(1)
 	}
 
-	resources := make(map[string]*Resource)
-
-	for _, f := range files {
-		file, parseDiags := parser.ParseHCLFile(f)
-		if parseDiags.HasErrors() {
-			fmt.Println(parseDiags.Error())
-			os.Exit(1)
-		}
-
-		for _, block := range reflect.ValueOf(file.Body).Elem().Interface().(hclsyntax.Body).Blocks {
-			if block.Type == "resource" || block.Type == "module" {
-				resource := decodeResource(block)
-				resources[resource.Name] = resource
-			}
+	for _, block := range reflect.ValueOf(file.Body).Elem().Interface().(hclsyntax.Body).Blocks {
+		if block.Type == "resource" || block.Type == "module" {
+			resource := decodeResource(block)
+			resources[resource.Name] = resource
 		}
 	}
 
